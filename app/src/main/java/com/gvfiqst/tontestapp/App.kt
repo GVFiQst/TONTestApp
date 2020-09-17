9fa8f2784451f5b1839e828a59d4d7dbf0483813
gvfiqst <gvfiqst@gmail.com>
2020-09-17 22:59:08 +0300

    Setup navigation component

--- app/src/main/java/com/gvfiqst/tontestapp/App.kt
+++ app/src/main/java/com/gvfiqst/tontestapp/App.kt
@@ -3,10 +3,9 @@ package com.gvfiqst.tontestapp
 import android.app.Application
 import com.gvfiqst.tontestapp.data.koin.dataModule
 import com.gvfiqst.tontestapp.domain.koin.domainModule
-import com.gvfiqst.tontestapp.presentation.koin.presentationModule
 import com.gvfiqst.tontestapp.koin.appModule
+import com.gvfiqst.tontestapp.presentation.koin.presentationModule
 import org.koin.android.ext.koin.androidContext
-import org.koin.androidx.fragment.koin.fragmentFactory
 import org.koin.core.context.startKoin
 
 
@@ -21,7 +20,6 @@ class App : Application() {
     private fun initKoinModules() {
         startKoin {
             androidContext(this@App)
-            fragmentFactory()
             modules(
                 appModule,
                 dataModule,
---
+++ presentation/src/main/java/com/gvfiqst/tontestapp/presentation/routing/MainNavigationImpl.kt
@@ -0,0 +1,8 @@
+package com.gvfiqst.tontestapp.presentation.routing
+
+import com.gvfiqst.tontestapp.presentation.main.MainActivity
+
+
+class MainNavigationImpl(
+    private val activity: MainActivity
+) : MainNavigation
